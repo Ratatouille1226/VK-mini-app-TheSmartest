92eb68e688aeaee3d079e4f54946974b91983a79
Andrey <kotlyar.andrey03@mail.ru>
2025-07-17 18:23:50 +0300

    Сверстал компонент квиза и логику, пока без стилей и с локальным хранилищем

--- frontend/src/pages/quiz/Quiz.tsx
+++ frontend/src/pages/quiz/Quiz.tsx
@@ -1,12 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import styles from './quiz.module.css';
 import { fetchQuestions } from '../../slice-fetch/fetchQuestions';
+import { Link } from 'react-router-dom';
 
 export const Quiz = () => {
 	const dispatch = useAppDispatch();
 	const type = useAppSelector((state) => state.type.type);
 	const { data: questions, status, error } = useAppSelector((state) => state.questions);
+
+	const [step, setStep] = useState<number>(0);
+	const question = questions[step]; //Текущий вопрос (определяем по индексу с помощью шага);
+	const [score, setScore] = useState<number>(0); //Счёт правильных ответов
+	const percentage = Math.round((step / questions.length) * 100); //Шкала прогресса сколько вопросов
 
 	useEffect(() => {
 		if (type) {
@@ -14,26 +20,43 @@ export const Quiz = () => {
 		}
 	}, [type, dispatch]);
 
+	const onClickVariant = (index: number) => {
+		console.log(index);
+		setStep(step + 1);
+
+		if (index === question.correct) {
+			setScore(score + 1);
+		}
+	};
+
 	if (status === 'loading') return <p>Загрузка вопросов...</p>;
 	if (status === 'failed') return <p>Ошибка: {error}</p>;
 
 	return (
 		<div className={styles['wrapper__quiz']}>
 			<div className={styles['container__quiz']}>
-				<div className={styles['progress']}></div>
-				<h1>Что такое ?</h1>
-				<ul>
-					{questions.map((q, i) => (
-						<li key={i}>
-							<h3>{q.title}</h3>
-							<ul>
-								{q.variants.map((v, idx) => (
-									<li key={idx}>{v}</li>
-								))}
-							</ul>
-						</li>
-					))}
-				</ul>
+				<div className={styles['progress']} style={{ width: `${percentage}%` }}></div>
+				{step !== questions.length ? (
+					<>
+						<h1>{question.title}</h1>
+						<ul>
+							{question.variants.map((ques, i) => (
+								<li onClick={() => onClickVariant(i)} key={i}>
+									{ques}
+								</li>
+							))}
+						</ul>
+					</>
+				) : (
+					<>
+						<span>
+							Вы завершили тест: правильных ответов {score} из {questions.length}
+						</span>
+						<Link onClick={() => setStep(0)} to="/">
+							На главную
+						</Link>
+					</>
+				)}
 			</div>
 		</div>
 	);
